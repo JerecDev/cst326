0aad3a5b34fb0ad264a81ed24efc3be90130a5bd
JerecDev <zachabbett@gmail.com>
2020-02-02 19:19:19 -0800

    test1

---
+++ Assets/ballMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+    public float ballSpeed = 10.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    bool start = true;
+    void Update()
+    { 
+
+        rb.velocity = Vector3.left * ballSpeed;
+    }
+}
