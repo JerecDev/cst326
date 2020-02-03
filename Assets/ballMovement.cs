c4152b9d128649d7c98e698fe028ee7b33ffe69e
JerecDev <zachabbett@gmail.com>
2020-02-03 12:28:01 -0800

    pongFinal
    final grade version

---
+++ Assets/ballMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+    public float ballSpeed = 10.0f;
+    public Rigidbody rb;
+    public int scoreL, scoreR = 0;
+    public float x, y, z;
+    public float zMovement = 7.5f;
+    Vector3 start = new Vector3(0f, .5f, 0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(-20f, 0, zMovement);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.position.x > 15)
+        {
+
+            scoreL++;
+            Debug.Log("Left has scored! Score: " + scoreL + " / " + scoreR);
+            if (scoreL >= 11)
+            {
+                Debug.Log("Game over, Left Wins!!");
+                scoreL = 0;
+                scoreR = 0;
+            }
+            rb.position = start;
+            zMovement *= -1;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(20f, 0, zMovement);
+        }
+
+        if (rb.position.x < -15)
+        {
+            scoreR++;
+            Debug.Log("Right has scored! Score: " + scoreL + " / " + scoreR);
+            if (scoreR >= 11)
+            {
+                Debug.Log("Game Over, Right Wins!!");
+                scoreL = 0;
+                scoreR = 0;
+            }
+            rb.position = start;
+            zMovement *= -1;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(-20f, 0, zMovement);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float increase = 2.5f;
+        if (collision.gameObject.name == "PaddleLeft" || collision.gameObject.name == "PaddleRight")
+        {
+                rb.AddForce(rb.velocity * increase);
+        }
+    }
+}
---
+++ Assets/manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+    GameObject leftPaddle, rightPaddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftPaddle = GameObject.Find("PaddleLeft");
+        rightPaddle = GameObject.Find("PaddleRight");
+    }
+
+    // Update is called once per frame
+    float paddleSpeed = 10f;
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            rightPaddle.transform.Translate(Vector3.forward * Time.deltaTime * paddleSpeed);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+           rightPaddle.transform.Translate(-1 * Vector3.forward * Time.deltaTime * paddleSpeed);
+
+        if (Input.GetKey(KeyCode.W))
+            leftPaddle.transform.Translate(Vector3.forward * Time.deltaTime * paddleSpeed);
+
+        if (Input.GetKey(KeyCode.S))
+            leftPaddle.transform.Translate(-1 * Vector3.forward * Time.deltaTime * paddleSpeed);
+
+    }
+}
