50497089c11842eb4a1c1e59429e96545a23f0ab
JerecDev <zachabbett@gmail.com>
2020-02-09 21:36:58 -0800

    hw2push
    unfinishedatm

--- Assets/ballMovement.cs
+++ Assets/ballMovement.cs
@@ -6,14 +6,17 @@ public class ballMovement : MonoBehaviour
 {
     public float ballSpeed = 10.0f;
     public Rigidbody rb;
-    public int scoreL, scoreR = 0;
+    public int scoreL, scoreR, shield = 0;
     public float x, y, z;
     public float zMovement = 7.5f;
+    float increase = 2.5f;
     Vector3 start = new Vector3(0f, .5f, 0f);
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
         rb.AddForce(-20f, 0, zMovement);
     }
 
@@ -52,14 +55,26 @@ public class ballMovement : MonoBehaviour
             rb.velocity = Vector3.zero;
             rb.AddForce(-20f, 0, zMovement);
         }
+
+        if (rb.position.x == 0 && shield != 0)
+        {
+            shield--;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        float increase = 2.5f;
         if (collision.gameObject.name == "PaddleLeft" || collision.gameObject.name == "PaddleRight")
         {
-                rb.AddForce(rb.velocity * increase);
+            audioSource.pitch = rb.velocity.x / 10;
+            audioSource.Play();
+            rb.AddForce(rb.velocity * increase);
         }
+        if (collision.gameObject.name == "Cylinder")
+        {
+            shield = 3;
+        }
+
+
     }
 }
